5d133ce5996190836d9141271bafffee2e4725c2
Kongooii <konglm41@gmail.com>
2023-12-05 21:20:29 +0700

    fix: prisma env

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { env } from 'process';
+// import { env } from 'process';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     super({
-      datasources: { db: { url: env.DATABASE_URL } },
+      datasources: {
+        db: {
+          url: 'postgresql://myuser:mypassword@localhost:5432/mydb?schema=public',
+        },
+      },
     });
   }
 }
