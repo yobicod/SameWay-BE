214cc5c26d164c50d0ba0a2789974d93c8defea2
visal-amity <yobicod.4u@gmail.com>
2023-12-07 15:18:47 +0700

    feat: allow cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
