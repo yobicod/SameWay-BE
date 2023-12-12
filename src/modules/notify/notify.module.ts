03d540a86c0b14660fa5e31202801e553381402d
yobicod <yobicod.4u@gmail.com>
2023-12-13 00:30:39 +0700

    refactor: remove unused import

--- src/modules/notify/notify.module.ts
+++ src/modules/notify/notify.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotifyController } from './notify.controller';
 import { NotifyService } from './noify.service';
-import { HttpWrapperModule } from 'src/modules/http/http.module';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
 
 @Module({
-  imports: [HttpWrapperModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [NotifyController],
   providers: [NotifyService],
 })
