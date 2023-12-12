fc84174c9a27de2cf0ccd22b493c762364c60af7
yobicod <yobicod.4u@gmail.com>
2023-12-13 00:22:49 +0700

    add http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotifyModule } from './modules/notify/notify.module';
-import { HttpWrapperModule } from './modules/http/http.module';
+
 import { DriverModule } from './modules/driver/driver.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     NotifyModule,
-    HttpWrapperModule,
     DriverModule,
     PrismaModule,
+    HttpModule,
   ],
   controllers: [AppController],
   providers: [AppService],
