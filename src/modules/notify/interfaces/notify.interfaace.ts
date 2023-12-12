ab26f3eeacdbda4f59405e64e2674eaa26d84167
yobicod <yobicod.4u@gmail.com>
2023-12-13 00:15:36 +0700

    Delete notify.interfaace.ts

--- src/modules/notify/interfaces/notify.interfaace.ts
+++
@@ -1,6 +0,0 @@
-import { IEmergencyNotificationDto } from '../dto/notify.dto';
-
-export interface INotifyService {
-  notifyLineChannel(req: IEmergencyNotificationDto): Promise<boolean>;
-  createNotificationLog(req: IEmergencyNotificationDto): Promise<boolean>;
-}
