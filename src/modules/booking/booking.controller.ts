d97e7ab555de6638a6206755076ed5fae4b9b12b
yobicod <yobicod.4u@gmail.com>
2023-12-28 21:51:38 +0700

    feat: booking controller call booking service

--- src/modules/booking/booking.controller.ts
+++ src/modules/booking/booking.controller.ts
@@ -1,23 +1,27 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { BookingService } from './booking.service';
 @ApiTags('booking')
 @Controller('booking')
 export class BookingController {
+  constructor(private bookingService: BookingService) {}
   @Get()
   async getAllBookings() {
-    return 'all';
+    return this.bookingService.getAllBooking();
   }
 
   @Get('get-booking-by-user-email/:email')
   async getBookingByUserEmail(@Param('email') email: string) {
-    return 'by-email';
+    return this.bookingService.getBookingByUserEmail(email);
   }
 
   @Post('create')
   async createBooking(@Body() createBookingInput: any) {
-    return 'create';
+    return this.bookingService.createBooking(createBookingInput);
   }
 
   @Patch('update')
-  async updateBooking(@Body() updateBookingInput: any) {}
+  async updateBooking(@Body() updateBookingInput: any) {
+    return 'this method will implement after this';
+  }
 }
