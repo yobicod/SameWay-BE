6ae0da306e074dfb14d8fbf8fbe4fe03227e5fb4
yobicod <yobicod.4u@gmail.com>
2023-12-28 21:45:06 +0700

    feat: update booking module to use prisma service

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BookingController],
-  providers: [BookingService]
+  providers: [BookingService],
 })
 export class BookingModule {}
